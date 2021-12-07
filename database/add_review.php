aa4b74ea7ac72c60ef3c45a9265fa9bc3643ac80
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2021-12-06 21:51:21 -0500

    Added review functionality

---
+++ database/add_review.php
@@ -0,0 +1,64 @@
+<?php 
+    session_start();
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if (isset($_SESSION['Email']) && !empty($_SESSION['Email'])) {
+            if ((isset($_GET['libraryRating'])) && !empty($_GET['libraryRating'])) {
+                if ((isset($_GET['libraryReview'])) && !empty($_GET['libraryReview'])) { 
+                    if ((isset($_GET['libraryName'])) && !empty($_GET['libraryName'])) {
+                        require 'config.php';
+                        $dsn = "mysql:host=$servername;dbname=$dbname;charset=UTF8";
+                        $libRating = $_GET['libraryRating'];
+                        $libReview = $_GET['libraryReview'];
+                        try {
+                            $pdo = new PDO($dsn, $username, $password);
+                            $stmt = $pdo->prepare("SELECT Id FROM Library WHERE Name = ?");
+                            $stmt->bindParam(1, $_GET['libraryName']);
+                            $stmt->execute();
+                            $libId = ($stmt->fetch())['Id'];
+
+                            $stmt = $pdo->prepare("SELECT * FROM Users Where Email = ?");
+                            $stmt->bindParam(1, $_SESSION['Email']);
+                            $stmt->execute();
+                            $results = $stmt->fetch();
+                            $name = $results['Name'];
+                            $userId = $results['Id'];
+
+                            $stmt = $pdo->prepare("INSERT INTO Reviews (LibraryId, UserId, Review, Rating)
+                            Values (?, ?, ?, ?)");
+                            $stmt->bindParam(1, $libId);
+                            $stmt->bindParam(2, $userId);
+                            $stmt->bindParam(3, $libReview);
+                            $stmt->bindParam(4, $libRating);
+                            $stmt->execute();
+
+                            $stmt = $pdo->prepare("SELECT * FROM Reviews WHERE LibraryId = ?");
+                            $stmt->bindParam(1, $libId);
+                            $stmt->execute();
+                            $resultsCount = $stmt->rowCount();
+                            $total = 0;
+                            if ($resultsCount > 0) {
+                                while ($row = $stmt->fetch()) {
+                                    $total += $row['Rating'];
+                                    print_r($row);
+                                    echo "<br></br>";
+                                }
+                            }
+                            $average = $total/$resultsCount;
+                            $stmt = $pdo->prepare("UPDATE Library SET Rating = ? WHERE Id = ?");
+                            $stmt->bindParam(1, $average);
+                            $stmt->bindParam(2, $libId);
+                            $stmt->execute();
+
+                            header('Location: ../individual_result.php?Library=' . $_GET['libraryName'] );
+                            exit();
+                        }
+                        catch (PDOException $e) {
+                            echo "Connection failed: " . $e->getMessage();
+                        }
+                        $pdo=null;
+                    } else {echo "Library not found"; }
+                } else {echo "No review found"; }
+            } else {echo "No rating found"; }
+        }  else {echo "You must be logged in"; }
+    } else {echo "Invalid Method"; }
+?>
--- individual_result.php
+++ individual_result.php
@@ -16,7 +16,6 @@
             $pd0=null;
         }
     } else {echo "Invalid Method";}
-
 ?>
 
 <!DOCTYPE html>
@@ -68,61 +67,68 @@
                 Reviews
             </h2>
             <!-- Reviews -->
-            <div class="card text-white bg-secondary mt-2 col-md-8 col-12 mx-auto bg-gradient mb-2">
-                <div class="card-body col-6 mx-auto w-0 text-start w-100">
-                    <h3>Jack Ryan</h3>
-                    <h4>4 stars</h4>
-                    <p>Not the worst library, it's okay. I would consider going again. They have 2 floors, the the 2nd floor is pretty nice.
-                        The first floor is a bit cramped so I wouldn't recommend that one.
-                    </p>
-                </div>
-            </div>
-            <div class="card text-white bg-secondary mt-2 col-md-8 col-12 mx-auto bg-gradient">
-                <div class="card-body col-6 mx-auto w-0 text-start w-100">
-                    <h3>John Wick</h3>
-                    <h4>1 stars</h4>
-                    <p>One of the worst libraries I have ever been to in my life. Cannot believe they have the audacity to call this a library. Barely passes for a building.
-                        My day is ruined and my disappointment is immeasurable. I am suing them for crimes against humanity!
-                    </p>
-                </div>
-            </div>
-            <div class="card text-white bg-secondary mt-2 col-md-8 col-12 mx-auto bg-gradient">
-                <div class="card-body col-6 mx-auto w-0 text-start w-100">
-                    <h3>Karen Karenson</h3>
-                    <h4>2 stars</h4>
-                    <p>Wow I had heard so much good things about this place, but I was so disappointed when I went and they wouldn't serve me food. Apparently they don't serve
-                        food in a library. They probably just hate me because I'm better than them. 
-                    </p>
-                </div>
-            </div>
-            <div class="card text-white bg-secondary mt-2 mb-5 col-md-8 col-12 mx-auto bg-gradient">
-                <div class="card-body col-6 mx-auto w-0 text-start w-100">
-                    <h3>Nice Guy</h3>
-                    <h4>5 stars</h4>
-                    <p>Best library I have been to in the world. If I could give it 10 stars, I would. Go there again??
-                        I live there now (but please keep this a secret, the staff don't know I'm living in the basement). 
-                    </p>
-                </div>
-            </div>
-            <div class="card text-white bg-secondary mt-2 mb-5 col-md-8 col-12 mx-auto bg-gradient" >
-                <div class="card-body col-6 mx-auto w-0 text-start w-100">
-                <form action="#" method="GET">
-                    <div class="col-md-2 col-4">
-                        <label for="libraryRating" class="form-label">Library Rating</label>
-                        <select class="form-select" aria-label="Select rating" name="libraryRating" id="libraryRating">
-                            <option value="1">1 Star</option>
-                            <option value="2">2 Star</option>
-                            <option value="3">3 Star</option>
-                            <option value="4">4 Star</option>
-                            <option selected value="5">5 Star</option>
-                        </select>
-                    </div>
+            <?php 
+                if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                    if ((isset($_GET['Library'])) && !empty($_GET['Library'])) {
+                        try {
+                            $pdo = new PDO($dsn, $username, $password);
+                            $stmt = $pdo->prepare("SELECT Id FROM Library WHERE Name = ?");
+                            $stmt->bindParam(1, $_GET['Library']);
+                            $stmt->execute();
+                            $libId = ($stmt->fetch())['Id'];
+
+                            $stmt = $pdo->prepare("SELECT * FROM Reviews Where LibraryId = ?");
+                            $results = $stmt->execute([$libId]);
+                            $resultsCount = $stmt->rowCount();
+                            if ($resultsCount > 0) {
+                                while ($row = $stmt->fetch()) {
+                                    
+                                    $getNameStmt = $pdo->prepare("SELECT * FROM Users Where Id = ?");
+                                    $getNameStmt->bindParam(1, $row['UserId']);
+                                    $getNameStmt->execute();
+                                    $reviewerName = ($getNameStmt->fetch())['Name'];
+                                    
+                                    ?>
+                                    <div class="card text-white bg-secondary mt-2 col-md-8 col-12 mx-auto bg-gradient mb-2">
+                                        <div class="card-body col-6 mx-auto w-0 text-start w-100">
+                                            <h3><?=$reviewerName?></h3>
+                                            <h4><?=$row['Rating'] ?> stars</h4>
+                                            <p><?=$row['Review']?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                <?php
+                                }
+                            }
+                        } 
+                        catch (PDOException $e) {
+                            echo "Connection failed: " . $e->getMessage();
+                        }
+                        $pd0=null;
+                    }
+                } else {echo "Invalid Method";}
+                if (isset($_SESSION['Email']) && !empty($_SESSION['Email'])) { ?>
+                    <div class="card text-white bg-secondary mt-2 mb-5 col-md-8 col-12 mx-auto bg-gradient" >
+                    <div class="card-body col-6 mx-auto w-0 text-start w-100">
+                    <form action="./database/add_review.php" method="GET">
+                        <div class="col-md-2 col-4">
+                            <label for="libraryRating" class="form-label">Library Rating</label>
+                            <select class="form-select" aria-label="Select rating" name="libraryRating" id="libraryRating">
+                                <option value="1">1 Star</option>
+                                <option value="2">2 Star</option>
+                                <option value="3">3 Star</option>
+                                <option value="4">4 Star</option>
+                                <option selected value="5">5 Star</option>
+                            </select>
+                        </div>
                         <label for="libraryReview" class="form-label">Library Review</label>
                         <textarea maxlength="2000" id="libraryReview" name="libraryReview" class="form-control mb-3" placeholder="Library Review" rows="4"></textarea>
-                    <input type="submit"/>
-                </form>
-                </div>
-            </div>
+                        <input type="hidden" id="libraryName" name="libraryName" value=<?=$library['Name']?>>
+                        <input type="submit"/>
+                    </form>
+                    </div>
+                </div> <?php
+                }?>
         </div>
     </div>
     <?php include './include/footer.php'; include 'login_form.php'; ?>
@@ -153,7 +159,6 @@
                     '<p>Latitude: <?=$library['Latitude']?>, Longitude: <?=$library['Longitude']?></p>'
                 }
             ]
-
             markers.forEach((marker)=>{
                 var Marker = new google.maps.Marker({
                     position: marker.coordinates,
