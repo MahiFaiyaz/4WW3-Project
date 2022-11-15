31651bd46b3b3cb4a83270a3f9d855254a2ffd23
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2022-11-14 23:03:25 -0500

    updated jquery

--- database/login.php
+++ database/login.php
@@ -1,5 +1,5 @@
 <head>
-  <script src="jquery-3.3.1.min.js"></script>
+  <script src="jquery-3.6.0.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 </head>
 <?php
