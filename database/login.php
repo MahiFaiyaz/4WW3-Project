7807f94fdccb02e78058da15a759146857f6d3e9
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2022-11-14 23:05:38 -0500

    updated jquery

--- database/login.php
+++ database/login.php
@@ -1,5 +1,5 @@
 <head>
-  <script src="jquery-3.6.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 </head>
 <?php
@@ -53,8 +53,7 @@
                         title: "Error",
                         text: "Email not found!",
                         icon: "error"
-                      })
-                    });
+                      });
                     document.location="$currentURL"
                     </script>
                     ';}                
