2f40fc25c9717a1d8115a7c4896fd4d93fbeaed5
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2022-11-14 23:10:50 -0500

    idk

--- database/login.php
+++ database/login.php
@@ -1,6 +1,6 @@
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <?php
     // Start session and include validate form
